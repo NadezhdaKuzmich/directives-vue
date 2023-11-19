7819822752a03a57733233ffb615a043002e20f5
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-11-20 00:11:31 +0100

    add project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="hello">Hello, {{ name }}</h1>
+    <h2>Directives:</h2>
+    <div class="example">
+      <h3>@click, v-if, v-else</h3>
+      <button @click="flag = !flag" class="btn">Click!</button>
+      <h4 v-if="flag">Hello!</h4>
+      <h4 v-else>Bye, bue!</h4>
+    </div>
+    <div class="example">
+      <h3>@click</h3>
+      <div class="calculator">
+        <button @click="count = count + 1" class="btn">+</button>
+        <p>{{ count }}</p>
+        <button @click="count = count - 1" class="btn">-</button>
+      </div>
+    </div>
+    <div class="example">
+      <div class="model">
+        <h3>v-model</h3>
+        <input type="text" v-model="text" />
+        <p class="input-data"><span class="bold">Input:</span> {{ text }}</p>
+      </div>
+    </div>
+    <div class="example-list">
+      <h3>v-for="card in arrayCard"</h3>
+      <Cards />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref } from "vue";
+import Cards from "./components/Cards";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Cards,
+  },
+  setup() {
+    const name = "Nadiia";
+    const flag = ref(true);
+    const count = ref(0);
+    const text = ref("");
+
+    return {
+      name,
+      flag,
+      count,
+      text,
+    };
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+img {
+  width: 100%;
+  object-fit: cover;
+}
+
+.container {
+  max-width: 1000px;
+  color: #383853;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
+  margin: auto;
+}
+
+.example {
+  background: #e1e3f1;
+  border-radius: 8px;
+  text-align: center;
+  padding: 20px;
+  margin: 20px 0;
+}
+
+.calculator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.btn {
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  background: #9b9be0;
+  border-radius: 8px;
+  border: none;
+  padding: 6px 12px;
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+}
+
+.btn:hover {
+  color: #9b9be0;
+  background: #fff;
+}
+
+.model {
+  max-width: 400px;
+  margin: auto;
+}
+
+input {
+  width: 100%;
+  font-size: 18px;
+  color: #343536;
+  border: 1px solid #9b9be0;
+  border-radius: 8px;
+  padding: 8px;
+}
+
+.input-data {
+  text-align: left;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.example-list {
+  padding: 20px 0;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="card" v-for="card in arrayCard" :key="card.name">
+    <img :src="card.src" :alt="card.alt" class="card-img" />
+    <p><span class="bold">Name:</span> {{ card.name }}</p>
+    <p><span class="bold">Age:</span> {{ card.age }}</p>
+  </li>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Card",
+  props: {
+    arrayCard: Array,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.card {
+  width: calc(100% / 2 - 10px);
+  background: #f0f2ff;
+  box-shadow: 1px 2px 8px #6059704a;
+  border-radius: 8px;
+  padding: 10px;
+}
+
+.card-img {
+  border-radius: 8px;
+}
+</style>
